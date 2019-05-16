6da930056e5ed72fe0854205681f7fdaa9459451
Stephon Mikell, Jr <stephon.mikell@gmail.com>
2019-05-16 10:11:00 -0400

    eod commit

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,55 +1,43 @@
 import React, { Component } from 'react';
 
 export class FetchData extends Component {
-  displayName = FetchData.name
+    displayName = FetchData.name
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
+    constructor(props) {
+        super(props);
+        this.state = { stories: [], loading: true };
 
-    fetch('api/SampleData/WeatherForecasts')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ forecasts: data, loading: false });
-      });
-  }
+        fetch('api/HackerNews/bestStories')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ stories: data, loading: false });
+            });
+    }
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+    static renderItemCard(item) {
+        return (
+            <div className={"col-sm-4"}>
+                <div className={"card"}>
+                    <div className={"card-body"}>
+                        <h5 className={"card-title"}>{item.title}</h5>
+                        <p className={"card-text"}>By: {item.by}</p>
+                        <a className={"btn btn-primary"} href={item.url}>See Full Story</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : this.state.stories.map(story => FetchData.renderItemCard(story));
 
-    return (
-      <div>
-        <h1>Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1>Today's Top Stories</h1>
+                {contents}
+            </div>
+        );
+    }
 }
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Col, Grid, Row } from 'react-bootstrap';
-import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   displayName = Layout.name
